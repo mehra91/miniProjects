eb7d053a669855b868f1ec9bd268df757e22d513
mehra91 <pm7300779625@gmail.com>
2025-10-07 18:56:24 +0530

    ignore files

--- colorChangeProjects/script.js
+++ colorChangeProjects/script.js
@@ -3,17 +3,17 @@
 
 
  btn.forEach((button)=>{
-    console.log(button)
+   //  console.log(button)
      button.addEventListener('click',(e)=>{
          console.log(e.target.id)
-         if (e.target.id === 'green'){
-            body.style.backgroundColor = e.target.id
-         }else {
-            body.style.backgroundColor = e.target.id
-         }
+             body.style.backgroundColor = e.target.id;
+
+         // if (e.target.id === 'green'){
+         //    body.style.backgroundColor = e.target.id
+         //    console.log('if');
+         // }else {
+         //    body.style.backgroundColor = e.target.id
+         // }
              
-          
-        
-    
      })
  }) 
