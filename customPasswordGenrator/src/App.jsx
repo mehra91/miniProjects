d5cc0206704cf3f195d2ce2e291fb97dc5912456
mehra91 <pm7300779625@gmail.com>
2025-10-20 22:18:07 +0530

    add layout

--- customPasswordGenrator/src/App.jsx
+++ customPasswordGenrator/src/App.jsx
@@ -2,7 +2,15 @@
  
  const App = () => {
    return (
-     <div>App</div>
+     <div className='  h-screen w-full  flex items-center justify-center '> 
+            <div className='bg-slate-600 h-20 w-md relative  flex items-center justify-center '>
+                  <input type="password" placeholder='password' className= ' rounded-2xl border h-10 w-xs flex items-center justify-center pl-20 ' />
+                  <button className='h-10 w-15 rounded-3xl bg-blue-600 absolute right-16'>
+                    copy
+                  </button>
+
+            </div>
+     </div>
    )
  }
  
